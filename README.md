6add566c4601abd12371b13fc164cee835dc2121
gen-ghub <renbenzhuang234@gmail.com>
2020-10-27 22:53:40 +0900

    バリデーションの条件分岐の追加

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 | Column             | Type    | Options                        |
 | ------------------ | ------- | ------------------------------ |
 | nickname           | string  | null: false                    |
-| email              | string  | null: false, unique:true       |
+| email              | string  | null: false                    |
 | encrypted_password | string  | null: false                    |
 | first_name         | string  | null: false                    |
 | second_name        | string  | null: false                    |
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,34 @@
 class User < ApplicationRecord
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :second_name, presence: true
-  validates :kana_first_name, presence: true
-  validates :kana_second_name, presence: true
   validates :birthday, presence: true
-  # validates :email, unique: true
+
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name
-    validates :second_name
+  validates :kana_first_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }, unless: :first_box?
+
+  def first_box?
+    first_name == ""
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ン一-]+\z/, message: '全角（カタカナ）を使用してください' } do
-    validates :first_name
-    validates :second_name
+  validates :kana_second_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' }, unless: :second_box?
+
+  def second_box?
+    second_name == ""
+  end
+
+  validates :kana_first_name, presence: true,format: { with: /\A[ァ-ン一-]+\z/, message: '全角（カタカナ）を使用してください' }, unless: :kana_first_box?
+
+  def kana_first_box?
+    kana_first_name == ""
+  end
+
+  validates :kana_second_name, presence: true,format: { with: /\A[ァ-ン一-]+\z/, message: '全角（カタカナ）を使用してください' }, unless: :kana_second_box?
+
+  def kana_second_box?
+    kana_second_name == ""
   end
 
 

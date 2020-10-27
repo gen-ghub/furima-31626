562420a8b680394bba9e238aacb363397e2982d9
gen-ghub <renbenzhuang234@gmail.com>
2020-10-27 18:46:27 +0900

    ログイン機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  # validates :nickname, presence: true
-  # validates :first_name, presence: true
-  # validates :second_name, presence: true
-  # validates :kana_first_name, presence: true
-  # validates :kana_second_name, presence: true
-  # validates :birthday, presence: true
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :second_name, presence: true
+  validates :kana_first_name, presence: true
+  validates :kana_second_name, presence: true
+  validates :birthday, presence: true
   # validates :email, unique: true
 
   devise :database_authenticatable, :registerable,

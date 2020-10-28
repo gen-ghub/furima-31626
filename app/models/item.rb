819d8d11cdd5cd3b9e23e6a1ec9fd9683d8a752f
gen-ghub <renbenzhuang234@gmail.com>
2020-10-28 16:30:33 +0900

    プルダウンの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :delivery
+  belongs_to_active_hash :day
+  belongs_to_active_hash :area
+  belongs_to_active_hash :status
+  belongs_to_active_hash :tag
+
+  belongs_to :user
+  has_one_attached :image
+
   with_options presence: true do
     validates :name
     validates :description
@@ -11,8 +21,11 @@ class Item < ApplicationRecord
     validates :image
   end
 
+  validates :delivery_id, numericality: { other_than: 1 }
+  validates :day_id, numericality: { other_than: 1 }
+  validates :area_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :tag_id, numericality: { other_than: 1 }
 
 
-  belongs_to :user
-  has_one_attached :image
 end

2ac252a521e559bc707e2c6d5d08cc41bf1cab40
gen-ghub <renbenzhuang234@gmail.com>
2020-10-31 17:05:31 +0900

    トークンの発行

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,27 @@
 const pay = () => {
-  console.log("カード情報トークン化のためのJavaScript");
+  Payjp.setPublicKey("pk_test_5601d058687650fd8776c547");
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("shopping[number]"),
+      cvc: formData.get("shopping[cvc]"),
+      exp_month: formData.get("shopping[exp_month]"),
+      exp_year: `20${formData.get("shopping[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
 };
 
 window.addEventListener("load", pay);
+
